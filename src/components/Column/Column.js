ca41179d71644214f1fa7d311ae4b0d8082e4835
Łukasz Sochacki <sochacki.luki@gmail.com>
2025-12-17 09:21:18 +0100

    Create store and modify data in List and Column components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,14 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
+import { use } from 'react';
 
 const Column = (props) => {
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.columnId === props.id)
+  );
+
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
@@ -10,11 +16,11 @@ const Column = (props) => {
         {props.title}
       </h2>
       <ul className={styles.card}>
-        {props.cards.map((card) => (
-          <Card key={card.id} title={card.title} />
+        {cards.map((card) => (
+          <Card key={card.id} {...card} />
         ))}
       </ul>
-      <CardForm columnId={props.id} action={props.action} />
+      <CardForm />
     </article>
   );
 };
