f3c742eb3adb36c003f0b8bf88003cc952fa679f
Łukasz Sochacki <sochacki.luki@gmail.com>
2025-12-18 11:50:00 +0100

    Add 'UPDATE_SEARCHSTRING' action and dispatch it in SearchForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,28 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+  const dispatch = useDispatch();
+
+  const [searchString, setSearchString] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+    setSearchString('');
+  };
+
   return (
-    <form className={styles.searchForm}>
-      <TextInput placeholder='Search...' />
+    <form onSubmit={handleSubmit} className={styles.searchForm}>
+      <TextInput
+        placeholder='Search...'
+        type='text'
+        value={searchString}
+        onChange={(event) => setSearchString(event.target.value)}
+      />
       <Button>
         <span className='fa fa-search' />
       </Button>
