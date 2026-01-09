5671d92e24a9f5bb584250d1d53da2b34a1a6f1b
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-01-09 11:40:14 +0100

    Group subreducers and add REMOVE_CARD function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,8 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
-import { toggleCardFavorite } from '../../redux/store';
+import { toggleCardFavorite } from '../../redux/cardsRedux';
 import { useDispatch } from 'react-redux';
+import { removeCard } from '../../redux/cardsRedux';
 
 const Card = ({ id, title, isFavorite }) => {
   const dispatch = useDispatch();
@@ -11,12 +12,22 @@ const Card = ({ id, title, isFavorite }) => {
     dispatch(toggleCardFavorite(id));
   };
 
+  const handleRemove = (event) => {
+    event.preventDefault();
+    dispatch(removeCard(id));
+  };
+
   return (
     <li className={styles.card}>
       {title}
-      <button className={styles.favoriteButton} onClick={handleFavorite}>
-        <i className={clsx(isFavorite ? 'fa fa-star' : 'fa fa-star-o')} />
-      </button>
+      <div className={styles.buttons}>
+        <button className={styles.favoriteButton} onClick={handleFavorite}>
+          <i className={clsx(isFavorite ? 'fa fa-star' : 'fa fa-star-o')} />
+        </button>
+        <button className={styles.removeButton} onClick={handleRemove}>
+          <i className={'fa fa-trash'} />
+        </button>
+      </div>
     </li>
   );
 };
